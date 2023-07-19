135a6ca3c195bf0d4e26608fc769e852916471e0
KALLA NAVYA <119601911+navya1220@users.noreply.github.com>
2023-07-19 22:22:46 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (62.55%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+    }
+        return -1;
+}
+}
