d89ec21e3d71440f41c7e27d2b8d5c2c96a428b5
Kalla Navya <119601911+navya1220@users.noreply.github.com>
2023-12-15 08:18:57 +0530

    Time: 7 ms (85.29%), Space: 11.2 MB (39.00%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+       int m = paths.size();
+       map<string ,int> mp;
+       for(int i=0;i<m;i++) {
+           mp[paths[i][0]]++;
+       }
+        for(int i=0;i<m;i++) {
+            if(mp[paths[i][1]]<1) {
+                return paths[i][1];
+            }
+        }
+        return " ";
+    }
+};
