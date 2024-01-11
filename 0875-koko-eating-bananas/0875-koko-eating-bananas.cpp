95e6b64d04902d52f340f77f61527724b5127ec7
Kalla Navya <119601911+navya1220@users.noreply.github.com>
2024-01-11 11:14:31 +0530

    Time: 27 ms (94.50%), Space: 19.5 MB (22.18%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,47 +1,34 @@
 class Solution {
     
 public:
-    long long findHours(vector<int> &nums,long long speed){
-     
-     long long totalHours=0;
-    
-    for(auto i:nums){
+    long long timetaken(vector<int>& piles, long long mid) {
+      long long totaltime = 0;
+   for(auto i:piles){
          
           long long val=i;
         
-        if(val<speed) totalHours+=1;
+        if(val<mid) totaltime+=1;
         
-        else if(val%speed==0) totalHours+=(val/speed);   
+        else if(val%mid==0) totaltime+=(val/mid);   
         
-        else totalHours+=(val/speed+1);
+        else totaltime+=(val/mid+1);
     }
-       
-       return totalHours;
-}
-
-int minEatingSpeed(vector<int>& nums, int h) {
-    
-    long long low=1;
-    long long high=*max_element(nums.begin(),nums.end());
-    
-       int ans=0;
-    
-    while(low<=high){
-        
-        long long mid=(low+high)/2;
-        
-        if(findHours(nums,mid)<=h){
-             ans=mid;
-            high=mid-1;
-        }
-        
-        else{
-           low=mid+1;
-        }
-        
+        return totaltime;
     }
-   
-    
-    return ans;
-}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long low=1;
+        long long  high = *max_element(piles.begin(), piles.end());
+        int ans = 0;
+        while(low<=high) {
+            long long mid = (low+high)/2;
+            if(timetaken(piles, mid)<=h) {
+                ans = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
 };
