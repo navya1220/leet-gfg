67c7a3296dcf482a296e588699e399b48d44cb94
Kalla Navya <119601911+navya1220@users.noreply.github.com>
2024-01-22 19:15:33 +0530

    Time: 2 ms (46.45%), Space: 7 MB (9.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int w = 1;
-        for(int i=1;i<=n/2;i++) {
-            double sum=1;
-            for(int j=i;j<2*i;j++) {
-                sum *= (double)(n-j)/(j-i+1);
-            }
-            w += sum;
-        }
-        return w;
+      int dp[n+1];
+      dp[0] = 1;
+      dp[1] = 1;
+      for(int i=2;i<=n;i++) {
+          dp[i] = dp[i-1] + dp[i-2]; 
+      }
+    return dp[n];
     }
 };
